f6988920f7c5e016f0a6d1b80d9f80daf022d2a6
Gmulti <thomasdeneulin@gmail.com>
2018-05-15 10:40:47 +0200

    Improve weglot functions

--- weglot-functions.php
+++ weglot-functions.php
@@ -1,9 +1,30 @@
 <?php
 
+/**
+ * Get all options
+ * @since 2.0
+ *
+ * @return array
+ */
 function weglot_get_options() {
 	return Context_Weglot::weglot_get_context()->get_service( 'Option_Service_Weglot' )->get_options();
 }
 
+/**
+ * Get option
+ * @since 2.0
+ * @param string $key
+ * @return any
+ */
 function weglot_get_option( $key ) {
 	return Context_Weglot::weglot_get_context()->get_service( 'Option_Service_Weglot' )->get_option( $key );
+}
+
+/**
+ * Get original language
+ * @since 2.0
+ * @return string
+ */
+function weglot_get_original_language() {
+	return weglot_get_option( 'original_language' );
 }
