8358997e311c46b262c9b94410769df51d6a7e40
Gmulti <thomasdeneulin@gmail.com>
2018-07-12 10:39:55 +0200

    Remove shortcode if not translatable

--- src/actions/front/class-shortcode-weglot.php
+++ src/actions/front/class-shortcode-weglot.php
@@ -18,7 +18,8 @@ class Shortcode_Weglot {
 	 * @since 2.0
 	 */
 	public function __construct() {
-		$this->button_services = weglot_get_service( 'Button_Service_Weglot' );
+		$this->button_services      = weglot_get_service( 'Button_Service_Weglot' );
+		$this->request_url_services = weglot_get_service( 'Request_Url_Service_Weglot' );
 
 		add_shortcode( 'weglot_switcher', [ $this, 'weglot_switcher_callback' ] );
 	}
@@ -30,6 +31,10 @@ class Shortcode_Weglot {
 	 * @return string
 	 */
 	public function weglot_switcher_callback() {
+		if ( ! $this->request_url_services->is_translatable_url() ) {
+			return;
+		}
+
 		return $this->button_services->get_html( 'weglot-shortcode' ); //phpcs:ignore
 	}
 }
