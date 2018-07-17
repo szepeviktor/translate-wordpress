5d1f4c2bc58fa563ecf469456f49466f44adfbdb
Gmulti <thomasdeneulin@gmail.com>
2018-07-17 17:24:41 +0200

    Update gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const webpack = require("webpack");
-const env = JSON.stringify(process.env.NODE_ENV) || "development";
+const env = process.env.NODE_ENV || "development";
 
 module.exports = {
 	entry: {
