fae30448f7ff66c0f848f95aafe0742e3bc2ef16
Gmulti <thomasdeneulin@gmail.com>
2018-07-27 09:37:48 +0200

    Prepare readme and version

--- weglot.php
+++ weglot.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version 2.0.2
+ * @version 2.0.3
  */
 
 /*
@@ -11,7 +11,7 @@ Author: Weglot Translate team
 Author URI: https://weglot.com/
 Text Domain: weglot
 Domain Path: /languages/
-Version: 2.0.2
+Version: 2.0.3
 */
 
 /**
@@ -27,7 +27,7 @@ if ( ! defined('ABSPATH')) {
 define('WEGLOT_NAME', 'Weglot');
 define('WEGLOT_SLUG', 'weglot-translate');
 define('WEGLOT_OPTION_GROUP', 'group-weglot-translate');
-define('WEGLOT_VERSION', '2.0.2');
+define('WEGLOT_VERSION', '2.0.3');
 define('WEGLOT_PHP_MIN', '5.4');
 define('WEGLOT_BNAME', plugin_basename(__FILE__));
 define('WEGLOT_DIR', __DIR__ );
