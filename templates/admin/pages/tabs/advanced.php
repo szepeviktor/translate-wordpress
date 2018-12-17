9360f5b21fc6e754a01f39708e569cc4c2279f91
Gmulti <thomasdeneulin@gmail.com>
2018-12-17 11:57:10 +0100

    Remove option wc reload cart on advanced

--- templates/admin/pages/tabs/advanced.php
+++ templates/admin/pages/tabs/advanced.php
@@ -213,24 +213,6 @@ foreach ( $languages as $key => $value ) {
 				</div>
 			</td>
 		</tr>
-		<?php if ($this->wc_active_services->is_active()): ?>
-			<tr valign="top">
-				<th scope="row" class="titledesc">
-					<label for="<?php echo esc_attr( $options_available['active_wc_reload']['key'] ); ?>">
-						<?php echo esc_html( $options_available['active_wc_reload']['label'] ); ?>
-					</label>
-				</th>
-				<td class="forminp forminp-text">
-					<input
-						name="<?php echo esc_attr( sprintf( '%s[%s]', WEGLOT_SLUG, $options_available['active_wc_reload']['key'] ) ); ?>"
-						id="<?php echo esc_attr( $options_available['active_wc_reload']['key'] ); ?>"
-						type="checkbox"
-						<?php checked( $this->options[ $options_available['active_wc_reload']['key'] ], 1 ); ?>
-					>
-					<p class="description"><?php echo esc_html( $options_available['active_wc_reload']['description'] ); ?></p>
-				</td>
-			</tr>
-		<?php endif; ?>
 	</tbody>
 </table>
 
