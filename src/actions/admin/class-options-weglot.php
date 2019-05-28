0dae9cd8f8ef85bc5d7720dbfb763728ee75ae9c
Remy <remy@weglot.com>
2019-05-28 09:37:06 +0200

    Removing comment

--- src/actions/admin/class-options-weglot.php
+++ src/actions/admin/class-options-weglot.php
@@ -209,10 +209,6 @@ class Options_Weglot implements Hooks_Interface_Weglot {
 			$options['excluded_blocks'] = [];
 		}
 
-		/*if ( ! isset( $options['auto_switch_fallback'] ) ) {
-			$options['auto_switch_fallback'] = 'en';
-		}*/
-
 		return $options;
 	}
 }
